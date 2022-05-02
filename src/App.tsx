c2d939fe45289d90e8c08b7770da8ad4b1e202fd
Pedro Coelho <pedrohenriquesc@gmail.com>
2022-05-02 11:35:58 -0300

    Configurações iniciais, TailwindCSS e componente Widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
+import Widget from './components/Widget'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-    </div>
-  )
+const App = () => {
+  return <Widget />
 }
 
 export default App
